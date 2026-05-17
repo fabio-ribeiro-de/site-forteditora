00e077e3383aa714246e7671cc09d11e917b921d
Fabio <97350292+fabio-ribeiro-de@users.noreply.github.com>
2026-05-17 15:49:56 -0300

    rev34
    Criação do player 'Cachinhos de Laurinha'.

---
+++ coroa_laurinha/js.laurinha/music-list.js
@@ -0,0 +1,21 @@
+let allMusic = [
+  {
+    name: "01: Cachinhos de Laurinha",
+    artist: "FortEditora",
+    img: "musica-1",
+    src: "musica-1",
+  },
+  {
+    name: "02: Cachinhos de Laurinha",
+    artist: "FortEditora",
+    img: "musica-1",
+    src: "musica-2",
+  },
+  {
+    name: "03: Cachinhos de Laurinha",
+    artist: "FortEditora",
+    img: "musica-1",
+    src: "musica-3",
+  }
+]
+
