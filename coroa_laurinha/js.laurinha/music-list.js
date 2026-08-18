064135460b09ff1976a42d9a233f9a2757d1d6bc
Fabio <97350292+fabio-ribeiro-de@users.noreply.github.com>
2026-08-18 13:52:51 -0300

    rev_51
    Adição de musicas e vídeos ao player a coroa de laurinha.

--- coroa_laurinha/js.laurinha/music-list.js
+++ coroa_laurinha/js.laurinha/music-list.js
@@ -12,5 +12,34 @@ let allMusic = [
     img: "musica-2",
     src: "musica-2",
     type: "audio"
+  },
+  {
+    name: "03: Cachinhos de Laurinha",
+    artist: "FortEditora",
+    img: "musica-2",
+    src: "musica-3",
+    type: "audio"
+  },
+  {
+    name: "04: Cachinhos de Laurinha",
+    artist: "FortEditora",
+    img: "musica-2",
+    src: "musica-4",
+    type: "audio"
+  },
+  {
+    name: "01: Vídeo Coroa de Laurinha",
+    artist: "FortEditora",
+    img: "musica-1",
+    src: "video-1",
+    type: "video",
+    url: "https://drive.google.com/file/d/14PEhJHpJCX6JDPUUKkRAbPTvmjK-hIXc/preview"
+  }, {
+    name: "01: Vídeo Coroa de Laurinha",
+    artist: "FortEditora",
+    img: "musica-1",
+    src: "video-1",
+    type: "video",
+    url: "https://drive.google.com/file/d/1aSnQWXBHlvcMi7qB6lfm_omkMJ3qN7Gw/preview"
   }
 ];
