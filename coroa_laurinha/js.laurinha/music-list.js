39f0dceef7145b985f209be16f9d3b00c21c952c
Fabio <97350292+fabio-ribeiro-de@users.noreply.github.com>
2026-09-25 17:59:09 -0300

    rev_54
    Alteração no player coroa de laurinha.
    - Troca da capa
    - Correção do nome do video 5 e 6

--- coroa_laurinha/js.laurinha/music-list.js
+++ coroa_laurinha/js.laurinha/music-list.js
@@ -9,33 +9,33 @@ let allMusic = [
   {
     name: "02: Cachinhos de Laurinha",
     artist: "FortEditora",
-    img: "musica-2",
+    img: "musica-1",
     src: "musica-2",
     type: "audio"
   },
   {
     name: "03: Cachinhos de Laurinha",
     artist: "FortEditora",
-    img: "musica-2",
+    img: "musica-1",
     src: "musica-3",
     type: "audio"
   },
   {
     name: "04: Cachinhos de Laurinha",
     artist: "FortEditora",
-    img: "musica-2",
+    img: "musica-1",
     src: "musica-4",
     type: "audio"
   },
   {
-    name: "01: Vídeo Coroa de Laurinha",
+    name: "05: Vídeo Coroa de Laurinha",
     artist: "FortEditora",
     img: "musica-1",
     src: "video-1",
     type: "video",
     url: "https://drive.google.com/file/d/14PEhJHpJCX6JDPUUKkRAbPTvmjK-hIXc/preview"
   }, {
-    name: "01: Vídeo Coroa de Laurinha",
+    name: "06: Vídeo Coroa de Laurinha",
     artist: "FortEditora",
     img: "musica-1",
     src: "video-1",
